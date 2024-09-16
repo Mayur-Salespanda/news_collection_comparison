54bd2f089c96468484fa287b1e0a6f5f31099ca2
Mayur Thapliyal <mayur.thapliyal@salespanda.com>
2024-09-16 18:01:24 +0530

    Fixed bug

--- pages/News.py
+++ pages/News.py
@@ -9,14 +9,14 @@ from datetime import datetime, timedelta
 
 
 
-tab1, tab2,tab3 = st.tabs(["GNews", "Bing News Search API","News Apis"])
+tab1, tab2,tab3,tab4 = st.tabs(["GNews", "Bing News Search API","News Apis","News AI"])
 with tab1:
     max_news =  st.slider("Select The Max NO Of News Article",min_value=1,max_value=100,step=1,value=30)
     freshness=st.selectbox("Select news Freshness ['Day', 'Week','Month'] ",['Day', 'Week','Month'])
     sort_by=st.selectbox("Sort By ['RelevanceRanking', 'Date'] ",["Relevance", 'Date'])
     search_term =  st.text_input(label="Search News",placeholder="Search News",max_chars=100,)
     if search_term: 
-        if st.button("Click to Search News "):
+        if st.button("Click to Search News ",key="search button 1"):
             get_news_and_put_download_button("GNews",search_term,freshness,max_news,sort_by)
 with tab2:
     max_news =  st.slider("Select The Max NO Of News Article.",min_value=1,max_value=100,step=1,value=30,key="max news slider 2")
@@ -24,7 +24,7 @@ with tab2:
     sort_by=st.selectbox("Sort By ['RelevanceRanking', 'Date'] ",["Relevance", 'Date'],key="sortby news slider 2")
     search_term =  st.text_input(label="Search News",placeholder="Search News",max_chars=100,key="search term news slider 2")
     if search_term: 
-        if st.button("Click to Search News "):
+        if st.button("Click to Search News ",key="search button 2"):
             get_news_and_put_download_button("Bing News Search API",search_term,freshness,max_news,sort_by)
 
 with tab3:
@@ -41,13 +41,16 @@ with tab3:
             language = st.selectbox("select language",["ar","de","en","es","fr","he","it","nl","no","pt","ru","sv","ud","zh",])
             sort_by=st.selectbox("Sort By ['RelevanceRanking', 'Date'] ",["relevancy", 'popularity','publishedAt'])
             if search_term:
-                if st.button("Click to Search News "):
+                if st.button("Click to Search News ",key="search button 3.1"):
                     all_news = newsapi.get_everything_in_news(search_term,from_date,to_date,language,sort_by,pageSize,page)
                     st.write(all_news)
         else:
             country = st.selectbox("select country",['us',"in"])
             category=st.selectbox("select the category",["business","entertainment","general","technology","health","science","sports"])
-            if st.button("Click to Search News "):
+            if st.button("Click to Search News ",key="search button 3.2"):
                 all_news = newsapi.get_top_headlines_news(search_term,country,category,pageSize,page)
                 st.write(all_news)
-            
+
+
+with tab4:
+    st.write("yet to implement")
